61d6501fced25a85bad72d505891f6a27a65ff24
Nitin <puneranitin@gmail.com>
2025-09-16 12:48:14 +0530

    fvf

---
+++ maxCostPermutation.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int solve(vector<int>&arr)
+{
+
+    //cost of the permutation = min length of contiguous subsegment that needs to be sorted so that entire population becomes sorted in ascending order
+
+
+    //we have array consisting of integers from 0 to n where no integer appears more than once
+    //replce zeroes with integers so that array becomes permutation
+    
+
+    //
+
+    //calculate the max possible cost of resulting permutation
+
+
+
+
+
+
+}
+
+int main()
+{
+
+    vector<int>arr = {1,0,4,0,5};
+    cout<<solve(arr)<<endl;
+
+    return 0;
+    
+}
---
+++ nonDescendingArray.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+
+#define ll long long
+
+using namespace std;
+
+ll goodSubsets(vector<int>&a,vector<int>&b)
+{
+    //a good subset is one in which after swapping,both arrays are sorted in non descending order
+
+    //we have to calculate the numnber of good subsets
+    
+    
+
+
+
+
+
+}
+
+
+
+int main()
+{
+
+    vector<int>a = {2,1,4};
+    vector<int>b = {1,3,2};
+
+    cout<<goodSubsets(a,b)<<endl;
+
+    return 0;
+    
+}
