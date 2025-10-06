ef4d731a42f3c0b1bc2f5ccc95845e732c7a4b14
Nitin <puneranitin@gmail.com>
2025-10-06 16:25:39 +0530

    Done with my 200th question :)

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,40 +1,59 @@
 #include<iostream>
 #include<vector>
+#include<unordered_map>
 using namespace std;
 
-int findJudge(int n, vector<vector<int>>& trust)
+int minSwapsCouples(vector<int>& row)
 {
-    //town judge trusts nobody
-    //everybody trusts the town judge and only such person exists
 
-    //trust[i] = [ai,bi] =>ai trusts person bi
-    //return -1 otherwise
+    //n couples sitting in 2n seats
+    //row[i] is the index of person sitting in the ith seat
 
-    if(n == 1)return 1;
+    //couples are numbered in order first couple being (0,1),second : (2,3) .....(2n-2,2n-1)
 
-    vector<int>trustsOut(n+1,0), trustedBy(n+1,0);
+    //return the min swaps required such that every couple is sitting side by side
 
-    for(auto& t:trust)
+    //couple number = personID/2
+    
+    //Swap: simple swapping of both persons
+
+
+    int n = row.size();
+    unordered_map<int,int>pos;
+
+    for(int i = 0;i< n;i++)
+    pos[row[i]] = i;
+
+    int swaps = 0;
+
+    for(int i = 0;i<n;i+=2)
     {
-        int a = t[0], b = t[1];
+        int first = row[i];
+        int partner = first ^ 1;//finding partner :since  couples differ by one bit only 
 
-        trustsOut[a]++;
-        trustedBy[b]++;
-    }
+        if(row[i+1] != partner)
+        {
+            swaps++;
+        
+        int partnerPos = pos[partner];
 
-    for(int i = 0 ;i<=n;i++)
-    {
-        if(trustsOut[i] == 0 && trustedBy[i] == n-1)
-        return i;
+        //swap person in position i+1 with person at position partnerPos
+        swap(row[i+1], row[partnerPos]);
+
+        //updating their positions
+        pos[row[partnerPos]] = partnerPos;
+        pos[row[i+1]] = i+1;
+
+        }
     }
-    return -1;
+    return swaps;
 }
 
 
 int main()
 {
-    vector<vector<int>>trust = {{1,2}};
-    cout<<findJudge(2,trust)<<endl;
+    vector<int>row = {0,2,1,3};
+    cout<<minSwapsCouples(row)<<endl;
 
     return 0;
 
