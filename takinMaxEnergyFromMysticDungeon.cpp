a8039510d81747c909039d8388ba6dfe20cd655c
Nitin <puneranitin@gmail.com>
2025-10-10 23:58:41 +0530

    just done 1 leetcode question

---
+++ takinMaxEnergyFromMysticDungeon.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int maximumEnergy(vector<int> &energy, int k)
+{
+    // after absorbing energy from magician i , we will be instantly transported to magician i+k and this will continue till i+k does not exist
+
+    // return the max possible energy gained
+
+    // and when yu reach certain magician you must take energy
+
+    // so find max energy we can gain
+
+    int n = energy.size();
+
+    vector < vector<long long> dp(n);
+
+    long long ans = LLONG_MIN;
+
+    for (int i = n - 1; i >= 0; i--)
+    {
+        if (i + k < n)
+            dp[i] = energy[i] + dp[i + k];
+        else
+            dp[i] = energy[i];
+        ans = max(ans, dp[i]);
+    }
+    return ans;
+}
+
+int main()
+{
+
+    vector<int> energy = {5, 2, -10, -5, 1};
+    cout << maximumEnergy(energy, 3) << endl;
+    return 0;
+}
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,66 +1,39 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
 
-long long minTime(vector<int>& skills, vector<int>& mana)
+
+int maximumEnergy(vector<int>& energy, int k)
 {
-    int n = skills.size();
-    int m = mana.size();
-    
-    // start[j] = start time for potion j at wizard 0
-    vector<long long> start(m, 0);
-    
-    // done[i] = time when wizard i finishes their current potion
-    vector<long long> done(n, 0);
-    
-    start[0] = 0;
-    
-    for(int j = 0; j < m; j++)
-    {
-        if(j > 0)
-        {
-            // Find minimum start time for potion j
-            // such that no wizard conflicts
-            start[j] = 0;
-            
-            for(int i = 0; i < n; i++)
-            {
-                // Time when potion j would arrive at wizard i
-                long long arrival = start[j];
-                for(int k = 0; k < i; k++)
-                {
-                    arrival += 1LL * skills[k] * mana[j];
-                }
-                
-                // Wizard i must be free (done with potion j-1)
-                long long must_start_after = done[i];
-                
-                if(arrival < must_start_after)
-                {
-                    // Delay start of potion j
-                    start[j] += (must_start_after - arrival);
-                    arrival = must_start_after;
-                }
-            }
-        }
-        
-        // Process potion j through all wizards
-        long long current_time = start[j];
-        for(int i = 0; i < n; i++)
-        {
-            current_time += 1LL * skills[i] * mana[j];
-            done[i] = current_time;
-        }
+    //after absorbing energy from magician i , we will be instantly transported to magician i+k and this will continue till i+k does not exist
+
+    //return the max possible energy gained
+
+    //and when yu reach certain magician you must take energy 
+
+
+    //so find max energy we can gain 
+
+    int n = energy.size();
+
+    vector<vector<long long>dp(n);
+
+    long long ans = LLONG_MIN;
+
+    for (int i = n - 1; i >= 0; i--) {
+        if (i + k < n)
+            dp[i] = energy[i] + dp[i + k];
+        else
+            dp[i] = energy[i];
+        ans = max(ans, dp[i]);
     }
-    
-    return done[n-1];
+    return ans;
 }
 
 int main()
 {
-    vector<int>skills = {1,5,2,4};
 
-    vector<int>mana= {5,1,4,2};
+    vector<int>energy = {5,2,-10,-5,1};
+    cout<<maximumEnergy(energy,3)<<endl;
+    return 0;
 
-    cout<<minTime(skills,mana)<<endl;
-    return 0;    
 }
