b34cc7694b6f5f01e4411fd43d767c14de7bdd41
Nitin <puneranitin@gmail.com>
2025-10-21 18:11:33 +0530

    Just done with today's leetcode but unable ot POTD,will do codeforces now

--- implementPower.cpp
+++ implementPower.cpp
@@ -1,25 +1,35 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 double myPow(double x, int n)
 {
-    //we have to implement out own power function
-    if(n == 0)return 1;
+    // we have to implement out own power function
+    if (n == 0)
+        return 1;
+    if (x == 1.00000)
+        return 1;
 
-    
+    long long exp = n;
+    if (exp < 0)
+    {
+        x = 1 / x;
+        exp = -exp;
+    }
 
+    double half = myPow(x, exp / 2);
+
+    if (exp % 2 == 0)
+        return half * half;
+    else
+        return half * half * x;
 }
 
 int main()
 {
 
     double x = 2.00000;
-    int n = 10;
+    int n = -2147483648;
 
-    double ans = myPow(x,n);
-
-    cout<<ans<<endl;
-
+    cout << myPow(x, n) << endl;
     return 0;
-    
 }
--- noCostTooGreatEasy.cpp
+++ noCostTooGreatEasy.cpp
@@ -1,6 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+const int MAX_N = 200000;
+vector<vector<int>> primeFactors(MAX_N + 1);
+
+void prePrimeFactors()
+{
+    for (int i = 2; i <= MAX_N; i++)
+    {
+        if (!primeFactors[i].empty())
+            continue; // since it must have been already been marked
+
+        for (int j = i; j <= MAX_N; j += i)
+        {
+            primeFactors[j].push_back(i);
+        }
+    }
+}
+
 int minCost(vector<int> &a, vector<int &> b)
 {
     // Select an integr i (1<=i<=n) and increase ai by 1, this has cost of bi
@@ -8,6 +25,12 @@ int minCost(vector<int> &a, vector<int &> b)
     // Determine total cost to make it so that two integers i,j where 1<i<j<n and gcd(ai,aj) > 1
 
     // Here bi is 1 for all i
+    int n = a.size();
+    map<int, int> count;
+    int ans = 2;
+    for (int i = 0; i < n; i++)
+    {
+    }
 }
 
 int main()
