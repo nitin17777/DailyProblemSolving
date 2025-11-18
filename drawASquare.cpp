3bb2126e7e7f15c46d4094f632ebfbc34e282367
Nitin <puneranitin@gmail.com>
2025-11-19 00:01:15 +0530

    gg

---
+++ drawASquare.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+bool is(ll l, ll r, ll d, ll u)
+{
+    // lu ur rd rl
+    //  root(l^2 + u^2)
+}
+
+ll main()
+{
+
+    ll t;
+    ;
+    cin >> t;
+
+    while (t--)
+    {
+
+        ll l, r, d, u;
+        cin >> l >> r >> d >> u;
+
+        cout << is(l, r, d, u) << "\n";
+    }
+    return 0;
+}
