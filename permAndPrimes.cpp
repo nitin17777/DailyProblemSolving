d3dbbe19798750e9c6f15cc6f4988fc187ef11fd
Nitin <puneranitin@gmail.com>
2025-12-13 23:16:12 +0530

    Done 1 more cf with help thouugh!!

--- permAndPrimes.cpp
+++ permAndPrimes.cpp
@@ -1,6 +1,5 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-
 
 vector<int> solve(int n)
 {
@@ -8,26 +7,28 @@ vector<int> solve(int n)
     In this problem, MEX = smallest x which doesn't occur in collection c
     Primality of an array a1,a2...an is defined as number of pairs (l,r) : 1 < l < r < n and MEX(al, ... , ar) is a prime number
 
-    
+
     Primality -> Aise kitne (l,r) ke pair honge jinke andr elemnents ka MEX prime hoga
     Now find the permutation with max possible primality among all permutations of 1 to n
 
 
     */
 
-    if(n == 1)return {1};
-    if(n== 2)return {1,2};
+    if (n == 1)
+        return {1};
+    if (n == 2)
+        return {1, 2};
 
     int c = 4;
-    vector<int>ans(n,0);
-    ans[0] = 2,ans[n/2]= 1,ans[n-1] = 3;
+    vector<int> ans(n, 0);
+    ans[0] = 2, ans[n / 2] = 1, ans[n - 1] = 3;
 
-    for(int i = 0 ; i < n;i++)
+    for (int i = 0; i < n; i++)
     {
-        if(ans[i] == 0)ans[i] = c++;
+        if (ans[i] == 0)
+            ans[i] = c++;
     }
     return ans;
-
 }
 
 int main()
@@ -36,18 +37,18 @@ int main()
     cin.tie(NULL);
 
     int t;
-    cin>>t;
-    while(t--)
+    cin >> t;
+    while (t--)
     {
         int n;
-        cin>>n;
+        cin >> n;
 
-        vector<int>ans = solve(n);
+        vector<int> ans = solve(n);
 
-        for(auto& an:ans)cout<<an<<" ";
+        for (auto &an : ans)
+            cout << an << " ";
 
-        cout<<endl;
+        cout << endl;
     }
     return 0;
-    
 }
