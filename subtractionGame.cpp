30716eab699eb3f4145f183e51083d8133e5124d
Nitin <puneranitin@gmail.com>
2025-12-13 23:38:17 +0530

    Done total of 3 cf questions now in total, done for the day.

---
+++ subtractionGame.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+int solve(int a, int b)
+{
+
+They take exactlya or b stones from the pile, the player who is unable to make the move loses
+
+//Find n such that palyer 2 has a winning strategy no matter the move chosen by player 1
+
+1,4
+
+//
+
+}*/
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b;
+        cin >> a >> b;
+        cout << a + b << endl;
+    }
+    return 0;
+}
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,34 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
-vector<int> solve(int n)
-{
     /*
-    In this problem, MEX = smallest x which doesn't occur in collection c
-    Primality of an array a1,a2...an is defined as number of pairs (l,r) : 1 < l < r < n and MEX(al, ... , ar) is a prime number
+int solve(int a,int b)
+{
+    They take exactlya or b stones from the pile, the player who is unable to make the move loses
+
+    //Find n such that palyer 2 has a winning strategy no matter the move chosen by player 1
 
     
-    Primality -> Aise kitne (l,r) ke pair honge jinke andr elemnents ka MEX prime hoga
-    Now find the permutation with max possible primality among all permutations of 1 to n
 
+    // 1,4 
 
-    */
+    //so n should just be greater than a
 
-    if(n == 1)return {1};
-    if(n== 2)return {1,2};
-
-    int c = 4;
-    vector<int>ans(n,0);
-    ans[0] = 2,ans[n/2]= 1,ans[n-1] = 3;
-
-    for(int i = 0 ; i < n;i++)
-    {
-        if(ans[i] == 0)ans[i] = c++;
-    }
-    return ans;
 
 }
+*/
+
 
 int main()
 {
@@ -39,15 +28,10 @@ int main()
     cin>>t;
     while(t--)
     {
-        int n;
-        cin>>n;
-
-        vector<int>ans = solve(n);
-
-        for(auto& an:ans)cout<<an<<" ";
-
-        cout<<endl;
+        int a,b;
+        cin>>a>>b;
+        cout<<a+1<<endl;
+    
     }
     return 0;
-    
 }
