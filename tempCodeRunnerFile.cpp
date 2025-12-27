3c571d48362473d45b969eb59c1e0b76d0d6d084
Nitin <puneranitin@gmail.com>
2025-12-27 22:05:13 +0530

    Done 1 cf also. Will get back to work properly from tomorrow. :)

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,39 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-string reverseVowels(string s)
+string die(int a,int b)
 {
-    //Reverse only the vowels present in the string 
 
-    unordered_set<char>vowels = {'a','e','i', 'o' ,'u','A','E','I', 'O' ,'U'};
-
-
-    int l = 0,r= s.size()-1;
-
-    while(l<r)
-    {
-        //Moving left until we find a vowel
-        while(l<r && vowels.find(s[l]) == vowels.end())l++;
-
-
-        while(l<r && vowels.find(s[r]) == vowels.end())r--;
-
-        if(l<r)
-        {
-            swap(s[l],s[r]);
-            l++;
-            r--;
-        }
-    }
-    return s;
+    
 }
+
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    cout<<reverseVowels("IceCreAm")<<endl;
+    int a,b;
+    cin>>a>>b;
+
+    int maxi = max(a,b);
+
+    int num = 7-maxi;
+    int den = 6;
+
+    int g = __gcd(num,den);
+
+    num /= g;
+    den/=g;
+
+    cout<< num << "/" << den<<endl;
+
     return 0;
-    
 }
