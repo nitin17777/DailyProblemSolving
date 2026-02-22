6ad3baf6b8bc1b68121d870336bc6b36e28f1466
Nitin <puneranitin@gmail.com>
2026-02-22 14:25:10 +0530

    Done 1 lc for now, will do 1 of cf

--- binaryGap.cpp
+++ binaryGap.cpp
@@ -1,24 +1,23 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int binaryGap(int n)
 {
-    //Return the longest distance bw 2 adjacent 1s
+    // Return the longest distance bw 2 adjacent 1s
 
     string s = bitset<32>(n).to_string();
     int m = s.size();
     int ans = 0;
 
-
-    for(int i = 0;i<m;i++)
+    for (int i = 0; i < m; i++)
     {
-        if(s[i] == '1')
+        if (s[i] == '1')
         {
-            for(int j = i+1;j<m;j++)
+            for (int j = i + 1; j < m; j++)
             {
-                if(s[j] == '1')
+                if (s[j] == '1')
                 {
-                    ans = max(ans,j-i);
+                    ans = max(ans, j - i);
                     break;
                 }
             }
@@ -27,13 +26,12 @@ int binaryGap(int n)
     return ans;
 }
 
-
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    cout<<binaryGap(22)<<endl;
+    cout << binaryGap(22) << endl;
 
     return 0;
 }
