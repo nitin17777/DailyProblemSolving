63f881929cdd84fadbffe9625fbe4034535954da
Nitin <puneranitin@gmail.com>
2026-03-16 23:08:51 +0530

    Completed the remaining question as well. Done for the day.

--- reduceArrToHalf.cpp
+++ reduceArrToHalf.cpp
@@ -1,20 +1,39 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int minSetSize(vector<int>& arr)
+int minSetSize(vector<int>& a)
 {
     int n = a.size();
 
     //We can choose a set of integers and remove all the occurences
 
-    //Return the min size of the set so that at least half of the integers of the array aar eremoved
-
-   vector<int>freq;
-    for(auto & x:arr)freq[x]++;
-
-    sort(freq.begin(),freq.end(),auto& )
+    //Return the min size of the set so that at least half of the integers of the array are removed
 
 
+    //pair the number and it's frequency
+
+    // Then push each pair into priority queue
+
+    //And start deleting from the top
+
+    unordered_map<int,int>freq;
+    for(auto & x:a)freq[x]++;
+
+    priority_queue<int>pq;
+
+    for(auto& x:freq)pq.push(x.second);
+
+    int removed = 0;
+    int count = 0;
+
+
+    while(removed < n/2)
+    {
+        removed += pq.top();
+        pq.pop();
+        count++;
+    }
+    return count;
 }
 
 int main()
