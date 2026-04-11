98cc28d9981b393b02c2f1819f63d4daad5c4706
Nitin <puneranitin@gmail.com>
2026-04-11 22:06:48 +0530

    Done 1 of both to maintain streak.

---
+++ happyNum.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int getNext(int n) {
+    int sum = 0;
+    while (n > 0) {
+        int digit = n % 10;
+        sum += digit * digit;
+        n /= 10;
+    }
+    return sum;
+}
+
+bool isHappy(int n) {
+    unordered_set<int> seen;
+
+    while (n != 1 && seen.find(n) == seen.end()) {
+        seen.insert(n);
+        n = getNext(n);
+    }
+
+    return n == 1;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    if (isHappy(n))
+        cout << "true";
+    else
+        cout << "false";
+
+    return 0;
+}
