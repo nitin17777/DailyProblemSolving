993d31eae55976d1fa90676f2678b08e54e2c6d4
Nitin <puneranitin@gmail.com>
2026-05-13 23:35:52 +0530

    Done 1 lc + 1cf,just maintaining strak during exams

--- maxAndMod.cpp
+++ maxAndMod.cpp
@@ -1,30 +1,50 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
+#define ll long long
 using namespace std;
 
-vector<int> solve(int n)
+vector<int>solve(int n)
 {
-    /*
-    max(pi - pi-1) mod i = i-1
 
-    Find the permutation of lenght n
-    If no such permutation exists, return -1
+    vector<int>ans;
 
+    if(n%2 == 0)
+    {
+        ans.push_back(-1);
+        return ans;
+    }
 
+    //fidn the permutation of length n such that max(pi,p i-1)mod i = i-1
 
-    */
+    ans.push_back(n);
+    for(int i = 1;i<n;i++)
+    {
+        ans.push_back(i);
+    }
+    return ans;
+
 }
 
+      
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
     int t;
-    cin >> t;
-    while (t--)
+    cin>>t;
+
+    while(t--)
     {
         int n;
-        cin >> n;
+        cin>>n;
+
+        vector<int>ans = solve(n);
+
+        for(auto & x:ans)cout<<x<<" ";
+
+        cout<<endl;
     }
+
     return 0;
 }
+
