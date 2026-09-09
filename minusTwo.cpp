378f6a8bd194c2a1287d50507e20c1fd573b14db
Nitin <puneranitin@gmail.com>
2026-09-09 23:50:44 +0530

    just 1 cf

--- minusTwo.cpp
+++ minusTwo.cpp
@@ -1,7 +1,6 @@
 #include<bits/stdc++.h>
 #define ll long long
 using namespace std;
-
       
 int main()
 {
@@ -11,20 +10,15 @@ int main()
     int t;
     cin>>t;
 
-    while(t--)
-    {
+     while (t--) {
         int n;
-        cin>>n;
-
-        vector<int>a(n);
-        for(auto& x:a)cin>>x;
-
+        cin >> n;
+        
         vector<int> vec(n); for (auto &x: vec) cin >> x;
         
         int mod_4[] {0, 0, 0, 0};
         for (auto &x: vec) mod_4[x%4]++;
         cout << max({mod_4[0], mod_4[2], mod_4[1] + mod_4[3]}) << "\n";
-    
     }
 
     return 0;
