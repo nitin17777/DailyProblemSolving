f086ba6b18a142b7a138eab8654c10bb2f0e0760
Nitin <puneranitin@gmail.com>
2026-09-21 16:44:56 +0530

    Just 1 cf 1lc till now

--- 2dTraveling.cpp
+++ 2dTraveling.cpp
@@ -1,6 +1,12 @@
 #include<bits/stdc++.h>
 #define ll long long
 using namespace std;
+const ll INF = 1e18;
+
+ll dist(vector<ll>&x,vector<ll>&y,int i,int j)
+{
+    return abs(x[i]-x[j]) + abs(y[i]-y[j]);
+}
       
 int main()
 {
@@ -12,8 +18,8 @@ int main()
 
     while(t--)
     {
-        int n,k,a,b;
-        cin>>n>>k>>a>>b;
+        ll n,k,s,t;
+        cin>>n>>k>>s>>t;
 
 
         //we can either fly directly to our destination or go by visiting each city
@@ -23,14 +29,38 @@ int main()
 
         //Find min cost of all air tickets if he can take any number of flight
 
-        vector<pair<int,int>>city(n+1);
-        for(auto &x:city)
-        {
-            cin>>x.first;
-            cin>>x.second;
-        }
-       
-    }
+        vector<ll>x(n+1),y(n+1);
+        for(int i =1;i<=n;i++)cin>>x[i]>>y[i];
+    
+        //First k cities are major cities
 
+
+        //if no majore cities are there then min cost would be |xi-xj| + [yi-yj]
+
+        //Going directly form A to C cannot be more costly than going like A->B->C
+
+        //Fly directly
+        ll ans = dist(x,y,s,t);
+
+        ll fromA=INF;
+        ll fromB=INF;
+
+        for(int i=1;i<=k;i++)
+        {
+            fromA = min(fromA,dist(x,y,s,i));
+            fromB = min(fromB,dist(x,y,t,i));
+
+        }
+        ans=min(ans,fromA+fromB);
+        cout<<ans<<'\n';
+    }
     return 0;
 }
+
+
+// 0 0
+// 1 -2
+// -2 1
+// -1 3
+// 2 -2
+// -3 -3
---
+++ findXValArr.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+vector<ll> resultArray(vector<int>& nums, int k)
+{
+    vector<ll>ans;
+    int n = a.size();
+
+    //In one operation : We can remove any overlapping  prefix in nums such that nums remains non empty
+
+    //Find the x value of nums -> 
+
+    //And return an array result of size k where result[x] = x value of nums : 0<=x<=k-1
+
+    // x value of nums ...products of remaining elements leave remainder x when divided by k
+    
+
+
+}
+      
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+   
+    vector<int>nums = {1,2,3,4,5};
+    cout<<resultArray(nums)<<endl;
+
+    return 0;
+}
